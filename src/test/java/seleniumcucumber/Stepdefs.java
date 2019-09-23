11bfd7412aefa6608c4ba858d595e179d9720645
Daphne Magsby <39204251+daphnemcrossbrowser@users.noreply.github.com>
2019-09-23 12:45:19 -0500

    Update Stepdefs.java

--- src/test/java/seleniumcucumber/Stepdefs.java
+++ src/test/java/seleniumcucumber/Stepdefs.java
@@ -6,7 +6,7 @@ import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-import org.junit.Assert;
+import org.junit.Assert.*;
 import org.openqa.selenium.remote.*;
 import org.openqa.selenium.*;
 import java.net.URL;
@@ -31,35 +31,30 @@ public class Stepdefs {
         caps.setCapability("name", "selenium cucumber");
         caps.setCapability("browserName", "Chrome");
         caps.setCapability("platform", "Windows 10");
-        caps.setCapability("version", "71");
         caps.setCapability("screenResolution", "1366x768");
         caps.setCapability("record_video", "true");
 
         driver = new RemoteWebDriver(new URL("http://" + username + ":" + authkey +"@hub.crossbrowsertesting.com:80/wd/hub"), caps);
     }
-    @Given("^I go to my ToDo App$")
+    @Given("I go to my ToDo App")
     public void I_go_to_my_todo_app() throws Throwable {
          driver.get("http://crossbrowsertesting.github.io/todo-app.html");
     }
 
-    @When("^I click on all todos$")
-    public void I_click_on_my_todos() throws Throwable {
+    @When("I archive all todos")
+    public void I_archive_all_todos() throws Throwable {
         driver.findElement(By.name("todo-1")).click();
         driver.findElement(By.name("todo-2")).click();
         driver.findElement(By.name("todo-3")).click();
         driver.findElement(By.name("todo-4")).click();
         driver.findElement(By.name("todo-5")).click();
-    }
-
-    @When("^I click archive$")
-    public void I_click_archive() throws Throwable {
         driver.findElement(By.linkText("archive")).click();
     }
 
-    @Then("^I should have no todos$")
+    @Then("I should have no todos")
     public void I_should_have_no_todos() throws Throwable {
         List elems =  driver.findElements(By.className("done-true"));
-        Assert.assertEquals(0, elems.size());
+        assertEquals(0, elems.size());
     }
 
     @After
